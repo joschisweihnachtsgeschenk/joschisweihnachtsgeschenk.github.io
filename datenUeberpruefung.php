ffdc03fed9527d15a788481d11e95b1a738fe151
joschisweihnachtsgeschenk <57036284+joschisweihnachtsgeschenk@users.noreply.github.com>
2019-10-29 23:30:27 +0100

    Update datenUeberpruefung.php

--- datenUeberpruefung.php
+++ datenUeberpruefung.php
@@ -1,1 +1,16 @@
 
+if (isset($_POST['submit'])) {} 
+$code = htmlspecialchars(trim($_POST['code']));
+$errors = [];
+$success = false;
+/**
+ *  input textfeld | name="name"
+ *  Typ: string
+ *  Kriterien: mindestens 3 Zeichen lang
+ */
+if (empty($code)) {
+  $errors[] = HTML_ERROR_START . 'Bitte gib den Code ein.' . HTML_ERROR_END;
+} else if (strlen($code) != Christmas19){
+  $errors[] = HTML_ERROR_START . 'Das ist leider nicht der richtige Code. Versuche es noch einmal.' . HTML_ERROR_END;
+}
+if (count($errors) === 0) {} 
