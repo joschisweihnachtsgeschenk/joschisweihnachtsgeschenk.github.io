a85f6770986457546e7f7d978706dfe32fac643f
joschisweihnachtsgeschenk <57036284+joschisweihnachtsgeschenk@users.noreply.github.com>
2019-12-18 13:37:09 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -10,14 +10,15 @@
   })
 
   // Preloader
-  "use strict";
-	function prealoader () {
+  
+	$(function prealoader () {
    if ($('#preloader_1').length) {
      $('#preloader_1').fadeOut(); // will first fade out the loading animation
      $('#loader-wrapper').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
      $('body').delay(350).css({'overflow':'visible'});
   };
- }
+ });
+	
 
   // Back to top button
   $(window).scroll(function() {
