a337369e03eaabdbf2aa29e9650ab10f5acef8a5
joschisweihnachtsgeschenk <57036284+joschisweihnachtsgeschenk@users.noreply.github.com>
2019-12-18 23:33:15 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,8 +6,8 @@
  function prealoader () {
    if ($('#preloader_1').length) {
      $('#preloader_1').fadeOut(); // will first fade out the loading animation
-     $('#loader-wrapper').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
-     $('body').delay(350).css({'overflow':'visible'});
+     $('#loader-wrapper').fadeOut('slow'); // will fade out the white DIV that covers the website.
+     $('body').css({'overflow':'visible'});
   };
  }
 	
