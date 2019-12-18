b0e9fa07b8cd1afbcdcc6766d0bb9b7dffc62d69
joschisweihnachtsgeschenk <57036284+joschisweihnachtsgeschenk@users.noreply.github.com>
2019-12-18 13:31:06 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -10,13 +10,13 @@
   })
 
   // Preloader
-  $(window).on('load', function () {
-    if ($('#preloader').length) {
-      $('#preloader').delay(100).fadeOut('slow', function () {
-        $(this).remove();
-      });
-    }
-  });
+  function prealoader () {
+   if ($('#preloader_1').length) {
+     $('#preloader_1').fadeOut(); // will first fade out the loading animation
+     $('#loader-wrapper').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
+     $('body').delay(350).css({'overflow':'visible'});
+  };
+ }
 
   // Back to top button
   $(window).scroll(function() {
